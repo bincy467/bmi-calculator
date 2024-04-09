3d2ed19769dfc61ceea0b13740fbda4c446297ee
bincy467 <sbincy467@gmail.com>
2024-04-09 20:58:46 +0530

    bmi

---
+++ BMI.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("calculate");
+
+btn.addEventListener('click', function () {
+
+    let height = document.querySelector('#height').value;
+    let weight = document.querySelector('#weight').value;
+
+    if (height == '' || weight == '') {
+        alert('Please fill out the input');
+        return;
+    }
+
+    height = height / 100;
+
+    let BMI = (weight / (height * height));
+    BMI = BMI.toFixed(2);
+
+    document.querySelector('#result').innerHTML = BMI;
+
+    let status = '';
+
+    if (BMI < 18.5) {
+        status = "underweight";
+    } else if (BMI >= 18.5 && BMI < 25) {
+        status = "Healthy";
+    } else if (BMI >= 25 && BMI < 30) {
+        status = "Overweight";
+    } else {
+        status = "obese";
+    }
+
+    document.querySelector('.comment').innerHTML = `Comment: You are <span id="comment">${status}</span>`;
+});
